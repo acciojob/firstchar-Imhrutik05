95dcc080fe221b3843f692b03329560032682acd
Hrutik Chaudhari <hrutikchaudhari05@gmail.com>
2025-08-12 15:13:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstChar(text) {
   // your code here
+	let result = '';
+	text = text.trim();
+	if (text.length > 0){
+		return text.charAt(0);
+	}
+	return result;
+	
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
 //const text = prompt("Enter text:");
-alert(firstChar(text));
+//alert(firstChar(text));
